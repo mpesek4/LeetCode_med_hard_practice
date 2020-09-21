d34c7911e87115166405172abd5be4f7d07b6e91
Michael Pesek <pesekm@dupage.edu>
2020-09-21 15:19:42 -0500

    added comments and runtime analysis

--- carPooling.py
+++ carPooling.py
@@ -1,18 +1,22 @@
 def carPooling(trips, capacity):
 
-    trips.sort(key=lambda x: x[1])
+    
     cap_arr = [0 for _ in range(1000)]
 
+''' Iterating over trips, we create a capacity array that stores how many people are added or removed at certain times'''
     for idx,trip in enumerate(trips):
         cmax = -1
         cmax = max(cmax,trip[2])
         cap_arr[trip[2]] -= trip[0]
         cap_arr[trip[1]]+= trip[0]
     current_cap = 0
+
+    ''' Now when we iterate over each time, we keep track of a current total capacity that needs to stay below 'capacity', which is updated with all additions and removals'''
     for i in range(0, cmax+1):
         current_cap+= cap_arr[i]
         if current_cap > capacity:
             return False
     return True
 
+'''runtime O(n) space O(n)   we could also do this in one pass if we sort the array runtime would be nlogn which might be better than our 2 loops'''
 print(carPooling(trips = [[2,1,5],[3,5,7]], capacity = 3))
--- subTreeWithAllDeepest.py
+++ subTreeWithAllDeepest.py
@@ -6,6 +6,8 @@ class TreeNode(object):
 
 def subtreeWithAllDeepest(root):
 
+
+    ''' This function recurses over every node in our tree and stores the height (+1) of the left and right subtrees'''
     def rec(curr_node,depth):
         if curr_node is None:
             return depth
@@ -14,10 +16,11 @@ def subtreeWithAllDeepest(root):
         curr_node.val = [depth,depth, old_val]
 
         curr_node.val[0] = rec(curr_node.left,depth+1)
-
         curr_node.val[1] = rec(curr_node.right,depth+1)
 
-        return max(curr_node.val[0],curr_node.val[1])
+        '''we are overriding val with a bit more information, idx 0 has left tree height, idx 1 has right tree height, idx 2 has the old value'''
+
+        return max(curr_node.val[0],curr_node.val[1]) # the trickiest part of this algo is recognizing we need to store the max of these two values for every node
 
     rec(root, 0)
 
@@ -25,6 +28,7 @@ def subtreeWithAllDeepest(root):
 
     current_max = -1
     current_idx = -1
+    # Simple BFS (dfs would also work) and we keep track of the deepest node where its left and right subtree have equal heights
     while len(myQ) > 0:
         curr = myQ.pop(0)
         if curr.val[0] == curr.val[1]:
@@ -38,15 +42,18 @@ def subtreeWithAllDeepest(root):
             myQ.append(curr.right)
     return current_idx
 
-n7 = TreeNode(7,None,None)
-n6 = TreeNode(6,None,None)
-n0 = TreeNode(0,None,None)
-n8 = TreeNode(8,None,None)
-n4 = TreeNode(4,None,None)
 
-n2 = TreeNode(2,n7,n4)
-n5 = TreeNode(5,n6,n2)
-n1 = TreeNode(1,n0,n8)
-n3 = TreeNode(3,n5,n1)
+'''commented for testing, runtimes is O(n) as we take one pass over all the nodes to calculate their subtree heights, then we need another pass to find the deepest one that satisfies
+the condition that its left and right subtree are the same height'''
+# n7 = TreeNode(7,None,None)
+# n6 = TreeNode(6,None,None)
+# n0 = TreeNode(0,None,None)
+# n8 = TreeNode(8,None,None)
+# n4 = TreeNode(4,None,None)
 
-print(subtreeWithAllDeepest(n3))
+# n2 = TreeNode(2,n7,n4)
+# n5 = TreeNode(5,n6,n2)
+# n1 = TreeNode(1,n0,n8)
+# n3 = TreeNode(3,n5,n1)
+
+# print(subtreeWithAllDeepest(n3))
