410603956309951f4be308e8867f483d22bf5e09
Michael Pesek <pesekm@dupage.edu>
2020-09-28 14:27:17 -0500

    medium level simulation problem, fairly easy but lots of details to consider

--- LC_816_med_ambiguousCoordinates.py
+++ LC_816_med_ambiguousCoordinates.py
@@ -1,5 +1,5 @@
 
-# https://leetcode.com/contest/weekly-contest-80/problems/ambiguous-coordinates/
+#https://leetcode.com/contest/weekly-contest-80/problems/ambiguous-coordinates/
 
 def ambiguousCoordinates(S):
     def zeroCheck(s):
@@ -11,32 +11,46 @@ def ambiguousCoordinates(S):
         
         leftHalf = s[0]
         rightHalf = s[1]
-        if len(leftHalf) > 1 and (leftHalf[0] == '0' and rightHalf[0] == '0'):
+        if len(leftHalf) > 1 and leftHalf[0] == '0':
             return False
         if rightHalf[-1] == '0':
             return False
         return True
     
-    def validPermutations(s):
+    def validPermutations(s,curr_half):
         #this function tries decimals in every location
-        # we have to check extraneous zeroes
-        ans = 1
+        #and if it is a valid one we add it to l or r, which is given by curr_half
+        ans = 0
+        if s[0] != '0':
+            ans = 1
+            curr_half.append(s)
+        elif len(s) == 1:
+            ans = 1
+            curr_half.append(s)
+        else:
+            ans = 0
         for i in range(1,len(s)):
-            s.insert(i,".")
-            if zeroCheck(s):
+            temp = s[:]
+            temp.insert(i,".")
+            if zeroCheck(temp):
+                curr_half.append(temp)
                 ans+=1
-            del s[i]
-        return ans
-                     
+        return ans                    
     s = list(S)
     s = s[1:-1]
-    
-    answer = 0
+    answer = [] 
+    #because we don't know where the numbers were split from left right, we try every combo of left halves and right halves by splitting at every index
     for i in range(1,len(s)):
         left = s[:i]
         right = s[i:]
+        l = []
+        r = [] 
         
-        numLeft = validPermutations(left)
-        numRight = validPermutations(right)
-        answer+= numLeft * numRight 
+        numLeft = validPermutations(left , l)
+        numRight = validPermutations(right , r)
+        # once we have all the valid permutations we just create the strings as stated in problem description, number of strings is numLeft*numRight (didnt end up using that value)
+        for left_item in l:
+            for right_item in r:
+                temp = "("+"".join(left_item)+", "+"".join(right_item)+")"
+                answer.append(temp)
     return answer
